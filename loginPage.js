80173376599279fe1005a4787961d4b474c382a8
Jervdv <jeremyvdvinne@gmail.com>
2024-05-14 21:13:32 -0300

    modified register to module and fixed register

--- loginPage.js
+++ loginPage.js
@@ -1,6 +1,4 @@
 import { User } from "./user.js"
-import { List } from "./list.js";
-import { Item } from "./item.js";
 
 window.onload = function () {
     if (localStorage.getItem("manter") == "true") {
@@ -12,27 +10,6 @@ window.onload = function () {
     } else {
         localStorage.setItem("loggeduser", null);
     }
-
-    //SOMENTE TESTES
-    var list = new List("nomeee")
-    var listas = []
-    var listArray
-    listas = [list]
-    listas[0].addItem(new Item("bola", "é uma bola"))
-    listArray = List.arrayFromJSON(JSON.parse(JSON.stringify(listas)))
-    console.log(listArray)
-    var user = new User("jer", 123, "mmailll")
-    user.addList(listas[0])
-    var users = []
-    users.push(user);
-    console.log(User.arrayfromJSON(JSON.parse(JSON.stringify(users))))
-
-
-    var users = [user]
-    //var jsonuser = JSON.parse(JSON.stringify(users))
-    // users = User.fromJSON(jsonuser, listArray)
-    //console.log(JSON.stringify(listas))
-
 } //da clear no usuario logado
 
 let loginForm = document.getElementById("loginForm");
@@ -47,7 +24,7 @@ loginForm.addEventListener("submit", (e) => {
     let userList = [];
     let userJson;
 
-    if (null === localStorage.getItem("users")) {
+    if (null == localStorage.getItem("users") || "null" == localStorage.getItem("users")) {
         const newUser = new User("Admin", "admin", "admin@email.com");
         userList.push(newUser);
         localStorage.setItem("users", JSON.stringify(userList));
@@ -66,23 +43,12 @@ loginForm.addEventListener("submit", (e) => {
                     passwordField.value = "";
                     username.value = "";
                     location.replace("index.html");
+                } else {
+                    alert("Senha incorreta!")
+                    password.value = "";
+                    passwordField.focus();
                 }
-            } else {
-                alert("Senha incorreta!")
-                password.value = "";
-                passwordField.focus();
             }
         }
-        /* if (passwordField.value == userList[usernameField.value].pw) {
-            localStorage.setItem("loggeduser", username.value);
-            localStorage.setItem("manter", cbmanter.checked);
-            password.value = "";
-            username.value = "";
-            location.replace("index.html");
-        } else {
-            alert("Senha Incorreta!");
-            password.value = "";
-            passwordField.focus();
-        } */
     }
 });
---
+++ registerPage.js
@@ -0,0 +1,51 @@
+import { User } from "./user.js";
+let loginForm = document.getElementById("cadastroForm");
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let usernameField = document.getElementById("username");
+    let passwordField = document.getElementById("password");
+    let emailField = document.getElementById("email");
+    let userJson, userList
+
+    let loginText = "";
+    if (null === localStorage.getItem("users")) {
+        const newUser = new User("Admin", "admin", "admin@email.com");
+        userList.push(newUser);
+        localStorage.setItem("users", JSON.stringify(userList));
+    }
+
+    loginText = localStorage.getItem("logins");
+    var loginJson = JSON.parse(loginText);
+
+    if (usernameField.value == "" || passwordField.value == "") {
+        alert("Insira valor em ambos os campos!");
+    } else {
+        let jaExiste = false;
+        userJson = JSON.parse(localStorage.getItem("users"));
+        userList = User.arrayfromJSON(userJson)
+        for (const user of userList) {
+            if (user.name == usernameField.value) {
+                usernameField.value = "";
+                alert("usuario ja existe!");
+                jaExiste = true;
+                usernameField.focus();
+            }
+        }
+
+        if (!jaExiste) {
+            userList.push(new User(
+                usernameField.value,
+                passwordField.value,
+                emailField.value
+            ))
+
+            localStorage.setItem("users", JSON.stringify(userList));
+            alert("cadastrado usario \"" + username.value + "\" com senha \"" + password.value + "\"");
+            password.value = "";
+            username.value = "";
+            window.location.replace('loginPage.html');
+        }
+    }
+});
